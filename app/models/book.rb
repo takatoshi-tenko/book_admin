79f2bce15dd65207c8e33a5dbc07bcc294404243
takatoshi.tenko <chanten811@gmail.com>
2024-04-29 18:39:31 +0900

    save

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,28 @@
 class Book < ApplicationRecord
+  enum sales_status: {
+    reservation: 0, # 予約受付
+    now_on_sale: 1, # 発売中
+    end_of_sale: 2, # 販売終了
+  }
+  scope :published, -> { where("price > ?", 3000) }
+  scope :written_about, -> { where("name like ?", "%#{theme}%") }
+  belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  validate do |book|
+    if book.name.include?("exercise")
+      book.errors[:name] << "I don't like exercise."
+    end
+  end
+
+  before_validation do
+    self.name = self.name.gsub(/Cut/) do |match|
+      "lovely (#{match})"
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_29_064107) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_29_090951) do
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "penname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "book_authors", force: :cascade do |t|
+    t.integer "book_id", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_book_authors_on_author_id"
+    t.index ["book_id"], name: "index_book_authors_on_book_id"
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.date "published_on"
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "publisher_id", null: false
+    t.integer "sales_status"
+    t.index ["publisher_id"], name: "index_books_on_publisher_id"
   end
 
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "book_authors", "authors"
+  add_foreign_key "book_authors", "books"
+  add_foreign_key "books", "publishers"
 end
